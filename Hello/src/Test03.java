25d5a4e0c1436d1fb0b8fdc8a5d89bc447719365
User <User@YD02-17>
2020-01-28 14:05:47 +0900

    IRORR

---
+++ Hello/src/Test03.java
@@ -0,0 +1,28 @@
+
+public class Test03 {
+	public static void main(String[] args) {
+
+		int sum = 0;
+		int i = 1;
+		while (i <= 100) {
+			if (i % 2 == 0 ) {
+			sum = sum + i;
+		}
+			i ++; 
+			System.out.println( "i: " + i + ", sum " + sum);
+
+		}
+		
+		int var1  = 5 ;
+		i = 1;
+	
+		while ( i <= 9) {
+			System.out.println(var1 + "*" +i +"= " + (var1*i));
+			i ++;
+		}
+			
+		
+	
+		
+	}// 메인
+}
